e9df947d3a9bc4bf9c9187e5f776df064c7bf46a
Ethan Smith <ejsmith@suu.edu>
2019-12-11 11:57:08 -0700

    day4Working

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -1,35 +1,41 @@
+# TODAY'S CHALLENGE: part 1: 5 minutes
+#                    part 2: 2 hours  :(
 def help():
     startingnum = 156218
     total = 0
     while (startingnum <= 652527):
-        total += isvalid(startingnum)
+        total += isvalid(str(startingnum))
         startingnum += 1
     print(total)
 
 def isvalid(num):
-    adjacent = 0
     decreases = False
+    adj = 0
     prev = -1
-    prev2 = -1
     for char in str(num):
         # this part is solid
         if int(char) < int(prev):
             decreases = True
-            break
-
-        if int(char) == int(prev):
-            adjacent += 1
-
-        if (adjacent > 0) and int(prev2) == int(char):
-            adjacent -= 1
-        if prev != -1:
-            prev2 = prev
         prev = char
 
+    for x in range(len(str(num))):
+        #print(num[x])
+        if num[x] == num[x-1]:
+            if num[x] != num[x-2]:
+                if (x+1 < len(str(num))):
+                    if num[x] != num[x+1]:
+                        adj+= 1
+                else:
+                    adj+=1
 
-    if (adjacent > 0) and not decreases:
-        return 1
+    if not decreases:
+        if adj > 0:
+        #    print ("" + num + ": is valid")
+            return 1
+        else:
+        #    print ("" + num + ": is NOT valid")
+            return 0
     return 0
 
-
 help()
+#print(isvalid('111111'))
