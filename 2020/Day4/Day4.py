6e008e1785a707a77a6f30ef5238d4fb3e14201a
Ethan Smith <ethan@esmithy.net>
2020-12-14 10:26:50 -0700

    finished day4

--- 2020/Day4/Day4.py
+++ 2020/Day4/Day4.py
@@ -1,3 +1,6 @@
+# this one was hard because I made a dumb mistake ( in the PID regex allowing for 10 characters worth of letters), and it took awhile to find
+# leaving all the comments that I used to debug :^)
+
 import re
 def getInput(fname):
     with open(fname) as f:
@@ -35,27 +38,55 @@ def checkPassports(lst):
             pport[f[0]] = f[1]
         #print (pport)
         if "byr" in pport and "iyr" in pport and "eyr" in pport and "hgt" in pport and "hcl" in pport and "ecl" in pport and "pid" in pport:
-            valid_count += 1# I dunno about this line lol
+            #valid_count += 1# I dunno about this line lol
+            valid_count += validate(pport)
+        # else:
+        #     print(pport)
+        #     print("failed by not having enough fields")
         # for part 2 we call validate instead of checking like this
     return valid_count
 
 def validate(d):
+    #print(" ")
+    print(d['pid'])
+    v = 0 # using this way to #print all errors in a line
     # if any tests fails, return 0, else return 1
-    if d['byr'] < 1920 or d['byr'] > 2002:
-        return 0
-    if d['iyr'] < 2010 or d['iyr'] > 2020:
-        return 0
-    if d['eyr'] < 2020 or d['eyr'] > 2030:
-        return 0
-    if d['hgt'][-2:]
+    if int(d['byr']) < 1920 or int(d['byr']) > 2002:
+        #print(d['byr'] +" failed on BYR")
+        v -= 1
+    if int(d['iyr']) < 2010 or int(d['iyr']) > 2020:
+        #print(d['iyr'] +" failed on iyr")
+        v -= 1
+    if int(d['eyr']) < 2020 or int(d['eyr']) > 2030:
+        #print(d['eyr'] + " failed on eyr")
+        v -= 1
+    if d['hgt'][-2:] == "in":
+        if int(d['hgt'][:-2]) < 59 or int(d['hgt'][:-2]) > 76:
+            #print(d['hgt'] +" failed on HGT_IN_bounds")
+            v -= 1
+    elif d['hgt'][-2:] == "cm":
+        if int(d['hgt'][:-2]) < 150 or int(d['hgt'][:-2]) > 193:
+            #print(d['hgt'] +" failed on HGT_CM_bounds")
+            v -= 1
+    else:
+        #print(d['hgt'] +" failed on HGT_units")
+        v -= 1 # this should catch a fully invalid height, not everything
+    if  not re.search("#[0-9a-f]{6}",d['hcl']): # something like this
+        #print(d['hcl'] + " failed on HCL")
+        v -= 1
+    if d['ecl'] not in ["amb","blu","brn","gry","grn","hzl","oth"]:
+        #print(d['ecl'] +" failed on ECL")
+        v -= 1
+    if not re.search("^[0-9]{9}$",d['pid']): # just a 9 digit number
+        print(d['pid'] +" failed on PID")
+        v -= 1
+    if v == 0:
+        #print("Valid pport")
+        return 1
+    #print(str(v) + "errors")
+    return 0
 
-    if d['hcl'] re.search(\#\w\w\w\w\w\w) # something like this
-
-    if d['ecl'] not in ['amb',"blu","brn","gry","grn","hzl","oth"]:
-        return 0
-    if d['pid'] # just a 9 digit number 
-
-
+# its not 148
 #print(getInput("example.txt"))
 # print(formatInput(getInput("example.txt")))
 print(checkPassports(formatInput(getInput("input.txt"))))
---
+++ 2020/Day4/f.py
@@ -0,0 +1,5 @@
+x = "gmt"
+if x not in ["amb","blu","brn","gry","grn","hzl","oth"]:
+	print("x not in the list")
+else:
+	print("x in the list")
