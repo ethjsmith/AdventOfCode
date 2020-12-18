7dc02e8d9e3ad3c73004bd02134814914ba9e4e2
Ethan Smith <ethan@esmithy.net>
2020-12-18 10:07:54 -0700

    Complete day7 ...wow

--- 2020/Day7/Day7.py
+++ 2020/Day7/Day7.py
@@ -1,8 +1,9 @@
-import sys
+import sys, re
 def getInput():
     with open(sys.argv[1]) as f:
         lst = f.readlines()
     lst = [x.strip() for x in lst]
+    lst = [x.strip(".") for x in lst]
     return lst
 
 # could use recursion here ?
@@ -15,21 +16,15 @@ def formatBags(): # explaining the whole process because this is confusing, and 
         action = component[1].split(",") # split the action into pieces for multipart actions
         tmp = []
         for i, ac in enumerate(action):
-
         #for ac in action:
-            if ac != " no other bags":
-                action[i] = ac[3:]
-
-            if ac[-3:] == "bag": ############################## HERE 
-                action[i] = ac + "s"
-        action[len(action)-1] = action[len(action)-1][:-1] # removes trailing period at the end of rules
-
-        # This part doesn't work right rn
-        # action_parts = []
-        # for entry in action: # split each portion of an action into the name, and the quantity
-        #     action_parts.append([entry[2:],entry[:3]]) # this is getting... messy
+            #print(ac)
+            tmp = ac
+            #if ac != " no other bags": # commenting this to keep the numbers, which are now needed
+            #    tmp = ac[3:]
+            if ac[-3:] == "bag":
+                tmp = tmp + "s"
+            action[i] = tmp
         formatted.append([component[0],action]) # add the formatted parts to a returnable lst
-        # action_parts = []
     return formatted
     #return would look like [['Condition'], [[action,number],[action2, number]]]]
 def makeRuleDict(): # turns this into a dictionary of rules and their links
@@ -37,26 +32,55 @@ def makeRuleDict(): # turns this into a dictionary of rules and their links
     rules = {}
     for rule in formatted:
         rules[rule[0]] = rule[1]
+    #for k, v in rules.items():
+        #print(k + ":" + str(v))
     return rules
 
-print(makeRuleDict())
-def canHoldt(t): # t can be our gold bag perhaps
-    rules = formatBags()
-    ways = 0
-    for rule in rules:
-        print(yeah)
-# so how do I actually solve this?
-# the question is, how many outerlevel bags can eventually hold a shiny gold bag?
+#print(makeRuleDict()) # testing
+
 
 def holdsgold (di, ind):
     counter = 0
     #for result in t[1]:
     for result in di[ind]:
-        if re.search("shiny gold bag",result):
+        print(result)
+        if re.search("shiny gold bags",result[3:]):
             counter += 1
-        elif re.search("no other bags", result):
+        elif re.search(" no other bags", result):
             counter += 0 # lol this is useless
         else:
-            counter += holdsgold(di,result)
+            counter += holdsgold(di,result[3:])
+    if counter > 1:
+        counter = 1
     return counter
 #print(formatBags())
+
+
+def canHoldt(): # t can be our gold bag perhaps
+    #rules = formatBags()#
+    rules = makeRuleDict()
+    ways = 0
+    for rule in rules:
+    #for i, rule in enumerate(rules):
+        print("######" + rule + "######")
+        ways += holdsgold(rules,rule)
+        #print(yeah)
+    return ways
+# so how do I actually solve this?
+# the question is, how many outerlevel bags can eventually hold a shiny gold bag?
+
+def howManyInside(di,ind):
+    count = 0
+    for result in di[ind]:
+        print(result)
+        if re.search(" no other bags", result):
+            count += 0 # for the bag containing no other bags
+        else:
+            loopnum = int(result[1])
+            while loopnum > 0:
+                loopnum -= 1
+                count += 1 + howManyInside(di,result[3:])
+    return count
+
+print(howManyInside(makeRuleDict(),"shiny gold bags")) # parts 2 ?
+#print(canHoldt())
